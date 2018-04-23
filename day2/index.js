a7cac0658386626c7ea1ae886afd26e01a037cec
mike1boost <sky2mike1@gmail.com>
2018-04-23 15:38:01 +0300

    kube

--- day2/index.js
+++ day2/index.js
@@ -1,1 +1,21 @@
-console.log("king");
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function myRandomKube(){
+    kube = ["a", "b", "c", "d", "e", "f"];
+
+    var index = getRandomInt(0,6);
+    return kube[index];
+}
+
+
+for (i=0;i<4;i++){
+    console.log(myRandomKube());
+}
+
+
+
